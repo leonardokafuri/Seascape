0c97f4eba447557737139adab6e4831a57f0293c
Rafael <rafapaivadeandrade>
2019-11-13 16:27:47 -0800

    Layout

--- app/src/main/java/com/example/seascapehotel/Register.java
+++ app/src/main/java/com/example/seascapehotel/Register.java
@@ -4,25 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
+    EditText  mEmail,mPassword, mFirstName,mLastName;
+    Button mRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mRegister = findViewById(R.id.register);
+        final String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        mFirstName = findViewById(R.id.firstName);
+        mLastName = findViewById(R.id.lastName);
+        mRegister = findViewById(R.id.register);
         Button regbtn = findViewById(R.id.register);
 
         regbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //add user to dabatase
+                //Authentication
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                if(!email.matches(emailPattern)){
+                    mEmail.setError("Invalid Email");
+                   mEmail.requestFocus();
+                    return;
+                }
 
-                //then go to new activity
-                startActivity(new Intent(Register.this,SignIn.class));
+               if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required");
+                    mPassword.requestFocus();
+                    return;
+                }
+               if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required");
+                    mEmail.requestFocus();
+                    return;
+                }
+
+               if(password.length() < 5){
+                    mPassword.setError("Password must be bigger or equals than 5 Characters");
+                    mPassword.requestFocus();
+                    return;
+                }
+              if(password.length() >= 25){
+                    mPassword.setError("Password must be lesser than 25 Characters");
+                    mPassword.requestFocus();
+                    return;
+                }
+                else{
+                    //add user to dabatase
+
+                    //then go to new activity
+                    startActivity(new Intent(Register.this,SignIn.class));
+                }
+
+
             }
         });
     }
--- app/src/main/java/com/example/seascapehotel/SignIn.java
+++ app/src/main/java/com/example/seascapehotel/SignIn.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SignIn extends AppCompatActivity {
 
@@ -13,16 +14,22 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
         Button forget = findViewById(R.id.ForgotPass);
-
+        Button signin = findViewById(R.id.signIn);
         forget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SignIn.this,Password.class ));
             }
         });
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
 
     }
 }
